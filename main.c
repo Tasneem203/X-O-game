917b3f370dffb97e791b95d6cfb2d272b9adc364
Tasneem203 <71629133+Tasneem203@users.noreply.github.com>
2020-09-26 16:06:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char arr[9] = {'1','2','3','4','5','6','7','8','9'};
+char arr[3][3] = {
+{'1', '2', '3'},
+{'4', '5', '6'},
+{'7', '8', '9'}};
 int iswinning();
 void print();
 
+int player = 1, i, userinput;
+char playnow;
 
 
 int main()
 {
-    int player = 1, i, userinput;
-    char playnow;
 
      do{
         print();
-
        if(player%2){
            player=1;}
         else{
@@ -23,45 +25,14 @@ int main()
      }
         printf("player %d choose a number to play: ", player);
         scanf(" %d", &userinput);
-         
-        if (player == 1){
-            playnow = 'X';
+                if (player == 1){
+            playnow ='X';
         }
          else{
              playnow = 'O';
          }
 
-        if (userinput == 1 && arr[0]== '1')
-        arr[0] = playnow;
-
-        else if (userinput == 2 && arr[1]== '2')
-        arr[1] = playnow;
-
-        else if (userinput == 3 && arr[2]== '3')
-        arr[2] = playnow;
-
-        else if (userinput == 4 && arr[3]== '4')
-        arr[3] = playnow;
-
-        else if (userinput == 5 && arr[4]== '5')
-        arr[4] = playnow;
-
-        else if (userinput == 6 && arr[5]== '6')
-        arr[5] = playnow;
-
-        else if (userinput == 7 && arr[6]== '7')
-        arr[6] = playnow;
-
-        else if (userinput == 8 && arr[7]== '8')
-        arr[7] = playnow;
-
-        else if (userinput == 9 && arr[8]== '9')
-        arr[8] = playnow;
-
-        else {
-        player--;
-        //getch();
-        }
+        insert();
 
         i=iswinning();
         player++;
@@ -79,38 +50,41 @@ int main()
 }
 
 
+
 int iswinning()
 {
-if (arr[0] == arr[1] && arr[1] == arr [2])
+
+if (arr[0][0] == arr[0][1]  && arr[0][1] == arr[0][2])
 return 1;
 
-else if (arr[3] == arr[4] && arr[4] == arr [5])
+else if (arr[1][0] == arr[1][1]  && arr[1][1] == arr[1][2])
 return 1;
 
-else if (arr[6] == arr[7] && arr[7] == arr [8])
+else if (arr[2][0] == arr[2][1]  && arr[2][1] == arr[2][2])
 return 1;
 
-else if (arr[0] == arr[3] && arr[3] == arr [6])
+else if (arr[0][0] == arr[1][0]  && arr[1][0] == arr[2][0])
 return 1;
 
-else if (arr[1] == arr[4] && arr[4] == arr [7])
+else if (arr[0][1] == arr[1][1]  && arr[1][1] == arr[2][1])
 return 1;
 
-else if (arr[2] == arr[5] && arr[5] == arr [8])
+else if (arr[0][2] == arr[1][2]  && arr[1][2] == arr[2][2])
 return 1;
 
-else if (arr[0] == arr[4] && arr[4] == arr [8])
+else if (arr[0][0] == arr[1][1]  && arr[1][1] == arr[2][2])
 return 1;
 
-else if (arr[2] == arr[4] && arr[4] == arr [6])
+else if (arr[0][2] == arr[1][1]  && arr[1][1] == arr[2][0])
 return 1;
 
-else if (arr [0] != '1' && arr [1] != '2' && arr [2] != '3' && arr [3] != '4' && arr [4] != '5' && arr [5] != '6' && arr [6] != '7' && arr [7] != '8' && arr [8] != '9')
+else if (arr[0][0] != '1' && arr[0][1] != '2' && arr[0][2] != '3' && arr[1][0] != '4' && arr[1][1] != '5' && arr[1][2] != '6' && arr[2][0] != '7' && arr[2][1] != '8' && arr[2][2] != '9')
 return 0;
 
 else
 return -1;
 }
+
 
 void print()
 {
@@ -119,12 +93,46 @@ printf("\n\n\t TIC TAC TEO\n\n");
 printf("Player 1 (X) - Player 2 (O)\n\n\n");
 
 printf("     |     |     \n");
-printf("  %c  |  %c  |  %c  \n", arr[0],arr[1], arr[2]);
+printf("  %c  |  %c  |  %c  \n", arr[0][0] ,arr[0][1], arr[0][2]);
 printf("_____|_____|_____\n");
 printf("     |     |     \n");
-printf("  %c  |  %c  |  %c  \n", arr[3], arr[4], arr[5]);
+printf("  %c  |  %c  |  %c  \n", arr[1][0] ,arr[1][1], arr[1][2]);
 printf("_____|_____|_____\n");
-printf("  %c  |  %c  |  %c  \n", arr[6],arr[7],arr[8]);
+printf("  %c  |  %c  |  %c  \n", arr[2][0] ,arr[2][1], arr[2][2]);
 printf("     |     |     \n");
-
 }
+
+
+int insert()
+{
+if (userinput == 1 && arr[0][0]== '1')
+arr[0][0] = playnow;
+
+else if (userinput == 2 && arr[0][1]== '2')
+arr[0][1] = playnow;
+
+else if (userinput == 3 && arr[0][2]== '3')
+arr[0][2] = playnow;
+
+else if (userinput == 4 && arr[1][0]== '4')
+arr[1][0] = playnow;
+else if (userinput == 5 && arr[1][1]== '5')
+arr[1][1] = playnow;
+
+else if (userinput == 6 && arr[1][2]== '6')
+arr[1][2] = playnow;
+
+else if (userinput == 7 && arr[2][0]== '7')
+arr[2][0] = playnow;
+
+else if (userinput == 8 && arr[2][1]== '8')
+arr[2][1] = playnow;
+
+else if (userinput == 9 && arr[2][2]== '9')
+arr[2][2] = playnow;
+
+else
+player--;
+//getch();
+}
+
